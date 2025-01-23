8888eacd0cdfcef32820de6d8c0592c650c8c84f
phirom-02 <phiromkhim02@gmail.com>
2025-01-23 18:00:18 +0700

    feat: configure pino-logger

---
+++ src/shared/middlewares/pino-logger.ts
@@ -0,0 +1,19 @@
+import { pinoLogger as logger } from "hono-pino";
+import pino from "pino";
+import pretty from "pino-pretty";
+
+import env from "@/env";
+
+export function pinoLogger() {
+  return logger({
+    pino: pino(
+      {
+        level: env.LOG_LEVEL || "info",
+      },
+      env.BUN_ENV === "production" ? undefined : pretty()
+    ),
+    http: {
+      reqId: () => crypto.randomUUID(),
+    },
+  });
+}
