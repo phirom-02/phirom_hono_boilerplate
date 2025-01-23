0050aa3096dca95757aed26dbcc32a0ea81e5bd0
phirom-02 <phiromkhim02@gmail.com>
2025-01-23 11:58:52 +0700

    feat: create Tasks schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
+import * as schema from "@/db/schema";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import * as schema from "@/db/schema";
 import env from "@/env";
 
 export const connection = postgres(env.DATABASE_URL, {
