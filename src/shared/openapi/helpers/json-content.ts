7f5d5bebca5eee94218bf7a83adc04a316846c94
phirom-02 <phiromkhim02@gmail.com>
2025-01-28 13:18:11 +0700

    feat: create open-api helpers

---
+++ src/shared/openapi/helpers/json-content.ts
@@ -0,0 +1,15 @@
+import type { ZodSchema } from "./types";
+
+export default function jsonContent<T extends ZodSchema>(
+  schema: T,
+  description: string
+) {
+  return {
+    content: {
+      "application/json": {
+        schema,
+      },
+    },
+    description: description,
+  };
+}
