628baf4b2d7b3023e3adb286df78f460e470e204
phirom-02 <phiromkhim02@gmail.com>
2025-01-28 13:41:33 +0700

    refactor: get validated request payload

--- src/modules/tasks/tasks-controller.ts
+++ src/modules/tasks/tasks-controller.ts
@@ -9,8 +9,7 @@ import {
 
 import { AppRouteHandler } from "@/shared/lib/types";
 import { HttpStatusCodes } from "@/shared/constants";
-import { CreateTaskPayload, UpdateTaskPayload } from "@/db/schema/tasks";
-import { ContentfulStatusCode } from "hono/utils/http-status";
+import { UpdateTaskPayload } from "@/db/schema/tasks";
 
 type TasksController = {
   getTasks: AppRouteHandler<GetTasksRoute>;
@@ -26,7 +25,7 @@ type TasksController = {
 
 const tasksController: TasksController = {
   async createTask(c) {
-    const createTaskPayload: CreateTaskPayload = await c.req.json();
+    const createTaskPayload = c.req.valid("json");
     const newTask = await tasksService.createTask(createTaskPayload);
 
     return c.json(newTask, HttpStatusCodes.OK);
@@ -38,29 +37,27 @@ const tasksController: TasksController = {
   },
 
   async getTaskById(c) {
-    const { id } = c.req.param();
-    const task = await tasksService.getTaskById(Number(id));
+    const { id } = c.req.valid("param");
+    const task = await tasksService.getTaskById(id);
 
     return c.json(task, HttpStatusCodes.OK);
   },
 
   async updateTask(c) {
-    const { id } = c.req.param();
+    const { id } = c.req.valid("param");
     const updateTaskPayload: UpdateTaskPayload = await c.req.json();
-    const updatedTask = await tasksService.updateTask(
-      Number(id),
-      updateTaskPayload
-    );
+    const updatedTask = await tasksService.updateTask(id, updateTaskPayload);
 
     return c.json(updatedTask, HttpStatusCodes.OK);
   },
 
   async deleteTask(c) {
-    const { id } = c.req.param();
+    const { id } = c.req.valid("param");
 
-    await tasksService.deleteTask(Number(id));
+    await tasksService.deleteTask(id);
 
-    return c.json(null, HttpStatusCodes.NO_CONTENT as ContentfulStatusCode);
+    // @ts-expect-error
+    return c.json(null, HttpStatusCodes.NO_CONTENT);
   },
 };
 
